167556c5dd00b43974536fcfc1972da296258d72
shukawam <shukawam@gmail.com>
2023-11-27 11:39:31 +0900

    Add option for basic authentication

--- src/main/java/com/example/myproject/AccountResource.java
+++ src/main/java/com/example/myproject/AccountResource.java
@@ -6,12 +6,18 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.opensearch.action.search.SearchRequest;
 import org.opensearch.action.search.SearchResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestClientBuilder;
+import org.opensearch.client.RestClientBuilder.HttpClientConfigCallback;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.index.query.QueryBuilders;
 import org.opensearch.search.builder.SearchSourceBuilder;
@@ -22,25 +28,31 @@ import java.io.IOException;
 @RequestScoped
 public class AccountResource {
     private String openSearchEndpoint;
+    private final CredentialsProvider credentialsProvider;
 
     @Inject
-    public AccountResource(@ConfigProperty(name = "oci.opensearch.api.endpoint") String openSearchEndpoint) {
-        Object endpoint =  System.getProperty("OPENSEARCH_ENDPOINT");
-        if(endpoint != null){
-            this.openSearchEndpoint = (String) endpoint;
-        }else{
-            this.openSearchEndpoint = openSearchEndpoint;
-        }
+    public AccountResource(@ConfigProperty(name = "oci.opensearch.api.endpoint") String openSearchEndpoint,
+            @ConfigProperty(name = "oci.opensearch.credential.username") String username,
+            @ConfigProperty(name = "oci.opensearch.credential.password") String password) {
+        this.openSearchEndpoint = openSearchEndpoint;
+        credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));
     }
 
     @Path("/search/{inputTerm}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response search(@PathParam("inputTerm") String inputTerm){
+    public Response search(@PathParam("inputTerm") String inputTerm) {
         // Create a client.
-        RestClientBuilder builder = RestClient.builder(HttpHost.create(openSearchEndpoint));
-
-        try(RestHighLevelClient client = new RestHighLevelClient(builder)){
+        RestClientBuilder builder = RestClient.builder(HttpHost.create(openSearchEndpoint))
+                .setHttpClientConfigCallback(new HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+        try (RestHighLevelClient client = new RestHighLevelClient(builder)) {
             // Build search request
             SearchRequest searchRequest = new SearchRequest("accounts");
 
